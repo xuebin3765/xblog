e209abcea324e3daf78e5ebdc84e24dff6ce0085
xuebin3765 <love3765>
2019-11-24 23:17:18 +0800

    增加标签列表页面

--- xblog-web-admin/js/cache.js
+++ xblog-web-admin/js/cache.js
@@ -1,5 +1,5 @@
-var cacheStr = window.sessionStorage.getItem("cache"),
-    oneLoginStr = window.sessionStorage.getItem("oneLogin");
+// var cacheStr = window.sessionStorage.getItem("cache"),
+//     oneLoginStr = window.sessionStorage.getItem("oneLogin");
 layui.use(['form','jquery',"layer"],function() {
     var form = layui.form,
         $ = layui.jquery,
