fe2815c60dd0bab0849ea9190f8eac47e41bcbb8
xuebin3765 <love3765>
2019-12-15 16:17:04 +0800

    修改富文本部分样式

--- xblog-web-admin/page/articles/articleAdd.js
+++ xblog-web-admin/page/articles/articleAdd.js
@@ -1,4 +1,4 @@
-layui.use(['form','layer','code','layedit','laydate','upload'],function(){
+layui.use(['form', 'layer', 'code', 'layedit', 'laydate', 'upload'], function () {
 
     var form = layui.form,
         layer = parent.layer === undefined ? layui.layer : top.layer,
@@ -10,11 +10,11 @@ layui.use(['form','layer','code','layedit','laydate','upload'],function(){
     var E = window.wangEditor;
 
     var editor = new E('#articleContent');
-    editor.customConfig.uploadImgServer = serverUrl+'/admin/fileUpload/photos';
+    editor.customConfig.uploadImgServer = serverUrl + '/admin/fileUpload/photos';
     // 关闭粘贴内容中的样式
-    editor.customConfig.pasteFilterStyle = false
+    editor.customConfig.pasteFilterStyle = false;
     // 忽略粘贴内容中的图片
-    editor.customConfig.pasteIgnoreImg = true
+    editor.customConfig.pasteIgnoreImg = true;
     // 使用 base64 保存图片
     //editor.customConfig.uploadImgShowBase64 = true
     // 上传图片到服务器
@@ -40,7 +40,7 @@ layui.use(['form','layer','code','layedit','laydate','upload'],function(){
             // insertImg 是插入图片的函数，editor 是编辑器对象，result 是服务器端返回的结果
             // 举例：假如上传图片成功后，服务器端返回的是 {url:'....'} 这种格式，即可这样插入图片：
             var data = result.data;
-            for (var i = 0; i < data.length; i++){
+            for (var i = 0; i < data.length; i++) {
                 insertImg(data[i].newFile);
             }
             // result 必须是一个 JSON 格式字符串！！！否则报错
@@ -49,16 +49,15 @@ layui.use(['form','layer','code','layedit','laydate','upload'],function(){
     };
     editor.create();
 
-
     //给分类 CheckBox赋值
-    $(document).ready(function(){
+    $(document).ready(function () {
         // 加载分类目录
         $.ajax({
             type: "GET",
-            url: serverUrl+'/admin/navigate/findAllNavigate',
+            url: serverUrl + '/admin/navigate/findAllNavigate',
             success: function (data) {
-                $.each(data.data, function(index,item) {
-                    $(".navigate_checkbox").append('<input id="navigate_item" type="checkbox" value="'+item.id+'" name="'+item.name+'" title="'+item.name+'" lay-skin="primary" />');
+                $.each(data.data, function (index, item) {
+                    $(".navigate_checkbox").append('<input id="navigate_item" type="checkbox" value="' + item.id + '" name="' + item.name + '" title="' + item.name + '" lay-skin="primary" />');
                 });
                 form.render();
             }
@@ -66,10 +65,10 @@ layui.use(['form','layer','code','layedit','laydate','upload'],function(){
         // 加载标签
         $.ajax({
             type: "GET",
-            url: serverUrl+'/admin/tag/findAllTag',
+            url: serverUrl + '/admin/tag/findAllTag',
             success: function (data) {
-                $.each(data.data, function(index,item) {
-                    $(".tag_checkbox").append('<input id="tag_item" type="checkbox" value="'+item.id+'" name="'+item.name+'" title="'+item.name+'" lay-skin="primary" />');
+                $.each(data.data, function (index, item) {
+                    $(".tag_checkbox").append('<input id="tag_item" type="checkbox" value="' + item.id + '" name="' + item.name + '" title="' + item.name + '" lay-skin="primary" />');
                 });
                 form.render();
             }
@@ -79,61 +78,61 @@ layui.use(['form','layer','code','layedit','laydate','upload'],function(){
     //上传缩略图
     upload.render({
         elem: '.thumbBox',
-        url: serverUrl+'/admin/fileUpload/photo',
-        method : "post",  //此处是为了演示之用，实际使用中请将此删除，默认用post方式提交
-        done: function(res, index, upload){
-            $('.thumbImg').attr('src',res.data.src);
-            $('.thumbBox').css("background","#fff");
+        url: serverUrl + '/admin/fileUpload/photo',
+        method: "post",  //此处是为了演示之用，实际使用中请将此删除，默认用post方式提交
+        done: function (res, index, upload) {
+            $('.thumbImg').attr('src', res.data.src);
+            $('.thumbBox').css("background", "#fff");
         }
     });
 
     //格式化时间
-    function filterTime(val){
-        if(val < 10){
+    function filterTime(val) {
+        if (val < 10) {
             return "0" + val;
-        }else{
+        } else {
             return val;
         }
     }
 
     //定时发布
     var time = new Date();
-    var submitTime = time.getFullYear()+'-'+filterTime(time.getMonth()+1)+'-'+filterTime(time.getDate())+' '+filterTime(time.getHours())+':'+filterTime(time.getMinutes())+':'+filterTime(time.getSeconds());
+    var submitTime = time.getFullYear() + '-' + filterTime(time.getMonth() + 1) + '-' + filterTime(time.getDate()) + ' ' + filterTime(time.getHours()) + ':' + filterTime(time.getMinutes()) + ':' + filterTime(time.getSeconds());
     laydate.render({
         elem: '#release',
         type: 'datetime',
-        trigger : "click",
-        done : function(value, date, endDate){
+        trigger: "click",
+        done: function (value, date, endDate) {
             submitTime = value;
         }
     });
 
-    form.on("radio(release)",function(data){
-        if(data.elem.title === "定时发布"){
+    form.on("radio(release)", function (data) {
+        if (data.elem.title === "定时发布") {
             $(".releaseDate").removeClass("layui-hide");
-            $(".releaseDate #release").attr("lay-verify","required");
-        }else{
+            $(".releaseDate #release").attr("lay-verify", "required");
+        } else {
             $(".releaseDate").addClass("layui-hide");
             $(".releaseDate #release").removeAttr("lay-verify");
-            submitTime = time.getFullYear()+'-'+(time.getMonth()+1)+'-'+time.getDate()+' '+time.getHours()+':'+time.getMinutes()+':'+time.getSeconds();
+            submitTime = time.getFullYear() + '-' + (time.getMonth() + 1) + '-' + time.getDate() + ' ' + time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
         }
     });
 
     form.verify({
-        articleName : function(val){
-            if(val === ''){
+        articleName: function (val) {
+            if (val === '') {
                 return "文章标题不能为空";
             }
         },
-        content : function(val){
+        content: function (val) {
             val = editor.txt.html();
-            if(val === ''){
+            if (val === '') {
                 return "文章内容不能为空";
             }
         }
     });
     var stick = $('.stick').val();
-    form.on('switch(articleTop)', function(data){
+    form.on('switch(articleTop)', function (data) {
         //开关value值，也可以通过data.elem.value得到
         stick = data.elem.checked;
     });
@@ -146,18 +145,18 @@ layui.use(['form','layer','code','layedit','laydate','upload'],function(){
         $("input:radio[name=status][value='1']").attr("checked", data.value === 1);
     });
 
-    form.on("submit(addArticle)",function(data){
+    form.on("submit(addArticle)", function (data) {
         //弹出loading
         ;
-        var index = top.layer.msg('数据提交中，请稍候',{icon: 16,time:false,shade:0.8});
+        var index = top.layer.msg('数据提交中，请稍候', {icon: 16, time: false, shade: 0.8});
         //将页面全部复选框选中的值拼接到一个数组中
         var navigate_checkbox = [];
-        $('.navigate_checkbox input[type=checkbox]:checked').each(function() {
+        $('.navigate_checkbox input[type=checkbox]:checked').each(function () {
             navigate_checkbox.push($(this).val());
         });
 
         var tag_checkbox = [];
-        $('.tag_checkbox input[type=checkbox]:checked').each(function() {
+        $('.tag_checkbox input[type=checkbox]:checked').each(function () {
             tag_checkbox.push($(this).val());
         });
         var content = editor.txt.html();
@@ -178,14 +177,14 @@ layui.use(['form','layer','code','layedit','laydate','upload'],function(){
         };
         $.ajax({
             type: 'POST',
-            url: serverUrl+'/admin/article/addOrUpdate',
+            url: serverUrl + '/admin/article/addOrUpdate',
             contentType: 'application/json;charset=utf-8',
             data: JSON.stringify(params),
             dataType: "json",
-            success:function (message) {
+            success: function (message) {
                 var mesg = message.msg;
                 var icon = 1;
-                if (0 !== message.code){
+                if (0 !== message.code) {
                     icon = 2;
                 }
                 top.layer.close(index);
@@ -194,7 +193,7 @@ layui.use(['form','layer','code','layedit','laydate','upload'],function(){
                 //刷新父页面
                 parent.location.reload();
             },
-            error:function (message) {
+            error: function (message) {
                 top.layer.close(index);
                 top.layer.msg("文章添加失败！");
                 layer.closeAll("iframe");
@@ -206,11 +205,10 @@ layui.use(['form','layer','code','layedit','laydate','upload'],function(){
     });
 
     //预览
-    form.on("submit(look)",function(){
+    form.on("submit(look)", function () {
         layer.alert("此功能需要前台展示，实际开发中传入对应的必要参数进行文章内容页面访问");
         return false;
     });
-
 
 
     // layui.layedit.set({
